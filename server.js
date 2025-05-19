05a3e093f473de628380b9a4de31abf292698a32
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-05-19 20:13:22 +0545

    add app.js and server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
