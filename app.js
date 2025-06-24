fc45e94f4a4041eb4e74294042c0443da2fc6506
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-24 17:58:53 +0545

    code formatting and cleanup

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // app.js
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/userRoutes");
 const adminUserRoutes = require("./routes/admin/adminUserRoutes");
 
 const app = express();
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 //server.js
 const dotenv = require("dotenv");
 const app = require("./app");
-const connectDB = require("./config/db");
 
 dotenv.config();
-
-connectDB();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+module.exports = app;
