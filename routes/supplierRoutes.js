9d9f6cc549dd9175c60e88de7002157e2227e736
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-16 15:08:04 +0545

    add supplier routes

---
+++ routes/supplierRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createSupplier,
+  getAllSuppliers,
+  getSupplierById,
+  updateSupplier,
+  deactivateSupplier,
+  activateSupplier,
+} = require("../controllers/supplierController");
+
+const {
+  authenticateUser,
+  isOwner,
+} = require("../middlewares/authenticateUser");
+const { create } = require("../models/Supplier");
+
+router.use(authenticateUser, isOwner);
+
+router.post("/create", createSupplier);
+router.get("/", getAllSuppliers);
+router.get("/supplier/:supplierId", getSupplierById);
+router.patch("/supplier/update/:supplierId", updateSupplier);
+router.patch("/supplier/deactivate/:supplierId", deactivateSupplier);
+router.patch("/supplier/activate/:supplierId", activateSupplier);
+
+module.exports = router;
