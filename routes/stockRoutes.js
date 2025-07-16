e4ad3c4e2c376e41a8c9e3c8181215d67ecd4c47
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-16 15:07:55 +0545

    add stockRoutes

---
+++ routes/stockRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllStock,
+  getStockByProductId,
+  getStockMovement,
+} = require("../controllers/stockController");
+
+const {
+  authenticateUser,
+  isOwner,
+} = require("../middlewares/authenticateUser");
+
+router.use(authenticateUser, isOwner);
+
+router.get("/", getAllStock);
+router.get("/product/:productId", getStockByProductId);
+router.get("/history/:productId", getStockMovement);
+
+module.exports = router;
